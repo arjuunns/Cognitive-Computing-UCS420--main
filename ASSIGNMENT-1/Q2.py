d2552645eb3bfff71bc47ffe7ab52d457792f258
arjuunns <singh.arjun2797@gmail.com>
2025-03-31 16:24:30 +0530

    all assg done

---
+++ ASSIGNMENT-1/Q2.py
@@ -0,0 +1,20 @@
+#2.1
+a=2
+b=3
+print(a+b)
+s=a+b
+print(s)
+
+#2.2
+string1="Arjun"
+string2="Singh"
+print(string1+" "+string2)
+full_name=string1+" "+string2
+print(full_name)
+
+#2.2
+name="Arjun Singh"
+roll=102317111
+print(name+" "+str(roll))
+result=name+" "+str(roll)
+print(result)
